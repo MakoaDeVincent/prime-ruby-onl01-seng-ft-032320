d6664fc7c199172c297efdd31ed87e4595983ec9
Makoa DeVincent <makoadevincent@gmail.com>
2020-03-26 07:10:43 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,11 @@
 # Add  code here!
-def prime?(number)
- if number <= 1 or number == 0 or value == 1
-   return false
- elsif
-   (2..number - 1).each do |i|
-     if number % i == 0
-       return false
-     end
-   end
- end
- return true
+def prime?(num)
+  if num <= 1
+    return false
+  elsif num <= 3
+    return true
+  else (2..num/2).none? do |n|
+    num % n == 0
+  end
+end
 end
