dc7077c85678f9e4bf485554ddeb9ab049b60f76
Makoa DeVincent <makoadevincent@gmail.com>
2020-03-26 07:07:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,5 +2,12 @@
 def prime?(number)
  if number <= 1 or number == 0 or value == 1
    return false
+ elsif
+   (2..number - 1).each do |i|
+     if number % i == 0
+       return false
+     end
+   end
  end
+ return true
 end
